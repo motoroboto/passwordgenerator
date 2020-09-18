51ef8675ed5a70631150fd93e7e58f7b64e8b6bf
motoroboto <robert.a.cullen@gmail.com>
2020-09-18 16:41:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+function options() {
+  var undefined;
+  if (confirm("Would You Like Your Password to Include Lower Case Letters?")) {
+    (confirm("Would You Like Your Password to Include Upper Case Letters?"))
+      (confirm("Would You Like Your Password to Include Numbers?")
+        (confirm("Would You Like Your Password to Include Special Characters?")
+          (prompt("How Many Characters Would You Like to Include in Your Password?"))))
+  }
+}  
+
+    
+  
+  
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// }
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
