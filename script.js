4dd6cd375792800b5768a8c3d57ad9b07ed28703
motoroboto <robert.a.cullen@gmail.com>
2020-09-19 13:34:23 -0400

    cleaning and drying js file where possible

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ var upperString = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
 var uppers = upperString.split(" ");
 var numberString = '1 2 3 4 5 6 7 8 9 0';
 var numbers = numberString.split(" ");
-var characterString = '! @ # $ % ^ & * ( )';
+var characterString = '~ ! @ # $ % ^ & * ( ) [ ] < > ? / \ + - =';
 var characters = characterString.split(" ");
-var passChars = '';
 
 function generatePassword () {
-  var lowerConfirm = confirm("Would You Like Your Password to Include Lower Case Letters?");
-  var upperConfirm = confirm("Would You Like Your Password to Include Upper Case Letters?");
-  var numberConfirm = confirm("Would You Like Your Password to Include Numbers?");
-  var characterConfirm = confirm("Would You Like Your Password to Include Special Characters?");
+  var passChars = '';
+  var lowerConfirm = confirm('Would You Like Your Password to Include Lower Case Letters?');
+  var upperConfirm = confirm('Would You Like Your Password to Include Upper Case Letters');
+  var numberConfirm = confirm('Would You Like Your Password to Include Numbers?');
+  var characterConfirm = confirm('Would You Like Your Password to Include Special Characters?');
   if (!lowerConfirm && !upperConfirm && !numberConfirm && !characterConfirm) {
-    alert("Please Choose At Least One Kind of Character For Your Password");
+    alert('Please Choose At Least One Kind of Character For Your Password');
     generatePassword ()
 }
-  var length = Number(prompt("How many characters would you like your password to be?", "8-128"));
-  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+  var length = Number(prompt('How many characters would you like your password to be?'));
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt('Length must be 8-128 characters. How many characters would you like your password to be?'));
 
 if (lowerConfirm) { passChars=passChars.concat(lowers); }
 if (upperConfirm) { passChars=passChars.concat(uppers); }
@@ -28,20 +28,18 @@ if (numberConfirm) { passChars=passChars.concat(numbers); }
 if (characterConfirm) { passChars=passChars.concat(characters); }
 var passCharacters = passChars.split(",")
 
-var password="";
-for(var i = 0; i < length; i++) {
-    var random = Math.floor(Math.random() * passCharacters.length);
-    password += passCharacters[random];
-}
-console.log(password);
-}
-  
+  var password='';
+  for(var i = 0; i < length; i++) {
+      var random = Math.floor(Math.random() * passCharacters.length);
+      password += passCharacters[random];
+      writePassword () 
+  }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword () {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
